fb1699157f791a7922940a2a297ca8406c367c0a
Adam Oresten <adamoresten@gmail.com>
2021-08-16 23:47:58 +0100

    Small updates

--- samples/simple-form/application.js
+++ samples/simple-form/application.js
@@ -4,13 +4,6 @@ export default class ExampleFormApplication extends AlpineFormApplication{
 
 	constructor(dialogData={}, options={}) {
 		super(dialogData, options);
-		this.items = [
-			"several",
-			"individual",
-			"strings",
-			"like",
-			"this"
-		]
 	}
 
 	/* -------------------------------------------- */
@@ -22,12 +15,29 @@ export default class ExampleFormApplication extends AlpineFormApplication{
 			template: `modules/alpinejs/samples/simple-form/template.html`,
 			classes: ["dialog"],
 			width: 150,
-			height: 150
+			height: 250
 		});
 	}
 
+	initializeAlpineData(){
+		return {
+			new_item: "",
+			items: [
+				"several",
+				"individual",
+				"strings",
+				"like",
+				"this"
+			],
+			addItem(){
+				this.items.push(this.new_item)
+				this.new_item = "";
+			}
+		};
+	}
+
 	_updateObject(){
-		console.log(this.items)
+		console.log(this.alpineData.items);
 	}
 
 }
--- scripts/alpine-form-application.js
+++ scripts/alpine-form-application.js
@@ -3,8 +3,13 @@ export default class AlpineFormApplication extends FormApplication{
 	constructor(dialogData={}, options={}) {
 		super(dialogData, options);
 		Hooks.once("renderAlpineFormApplication", () => {
-			this.form.setAttribute("x-data", `ui.windows[${this.appId}]`);
+			this.alpineData = this.initializeAlpineData();
+			this.form.setAttribute("x-data", `ui.windows[${this.appId}].alpineData`);
 		})
+	}
+
+	initializeAlpineData(){
+		return {};
 	}
 
 }
